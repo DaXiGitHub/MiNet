a9fcacb1651b30a2b7dae086facb609607cdf751
admin <admin@qq.com>
2019-02-02 14:28:53 +0800

    第一次提交

---
+++ src/main/java/com/demo/pojo/Person.java
@@ -0,0 +1,13 @@
+package com.demo.pojo;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -390369723271130463L;
+
+	
+}
---
+++ src/test/java/test/com/demo/AppTest.java
@@ -0,0 +1,5 @@
+package test.com.demo;
+
+public class AppTest {
+
+}
