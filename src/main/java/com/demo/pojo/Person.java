557643f17fe42a0de665149d115fd2dc0f4ae1ce
admin <admin@qq.com>
2019-02-02 16:06:03 +0800

    no

--- src/main/java/com/demo/pojo/Person.java
+++ src/main/java/com/demo/pojo/Person.java
@@ -9,5 +9,5 @@ public class Person implements Serializable {
 	 */
 	private static final long serialVersionUID = -390369723271130463L;
 
-	
+	private String id;
 }
--- src/test/java/test/com/demo/AppTest.java
+++ src/test/java/test/com/demo/AppTest.java
@@ -2,4 +2,7 @@ package test.com.demo;
 
 public class AppTest {
 
+	public static void main(String[] args) {
+		System.out.println("!");
+	}
 }
